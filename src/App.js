39b347f7d0b3dbd6adcb73f24f94508be1251151
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-25 10:36:28 +0800

    App have been modified

--- src/App.js
+++ src/App.js
@@ -9,6 +9,25 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true);
   const[tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true)
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+   
+  };
+  useEffect(() => {
+    fetchTours();
+  }, [])
+
   if(loading){
     return(
       <main>
@@ -16,7 +35,11 @@ function App() {
       </main>
     )
   }
-  return <h2>Tours Project Setup</h2>
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  )
 }
 
 export default App 
